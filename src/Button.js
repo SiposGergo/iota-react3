e53c303d91b80f5806b982ff469a648b41fdb685
Sipos Gergő <prosipinho@gmail.com>
2020-12-02 14:00:52 +0100

    Button clicked counter

--- src/Button.js
+++ src/Button.js
@@ -3,27 +3,24 @@ import Logo from './facebook.svg'
 
 
 export default class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.buttonRef = React.createRef();
-    }
+    state = { clickCount: 0 }
 
-    componentDidMount() {
-        if (this.props.id === 1)
-            this.buttonRef.current?.focus();
+    onButtonClicked = () => {
+        // this.setState({ clickCount: this.state.clickCount + 1 })
+        this.setState((state) => ({ clickCount: state.clickCount + 1 }))
     }
 
     render() {
         const { children, hidden, text, startIcon, endIcon, disabled } = this.props;
         return hidden ? null :
             <button
+                onClick={this.onButtonClicked}
                 disabled={disabled}
-                ref={this.buttonRef}
                 className={`btn ${disabled ? "disabled" : ''}`}
             >
                 {startIcon && <img src={Logo} alt="icon" />}
-                <span>{text ?? children}</span>
+                <span>{text ?? children} -{this.state.clickCount}</span>
                 {endIcon && <img src={Logo} alt="icon" />}
-            </button >
+            </button>
     }
 }
